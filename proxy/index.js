6684a1909b158c54081ba6473243afe496bc6071
henry cui <henllyee@126.com>
2015-01-09 09:25:14 +0800

    add proxy index

---
+++ proxy/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hengliang.cui on 2015/1/9.
+ */
+exports.Content = require('./content');
+exports.Customer = require('./customer');
+exports.Require = require('./require');
+exports.User = require('./User');
+
+
