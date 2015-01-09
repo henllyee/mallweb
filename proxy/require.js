e5581c2cdc626769dc023c38e9f1ac1c2f1b12e3
henry cui <henllyee@126.com>
2015-01-09 09:35:50 +0800

    require proxy modify

--- proxy/require.js
+++ proxy/require.js
@@ -21,5 +21,19 @@ function addRequire(data,callback){
 function addCustomer(data,callback){
     var model = new Customer();
     model.company_name=data.company_name;
+    model.company_address = data.company_address;
+    model.company_fax = data.company_fax;
+    model.company_tel = data.company_tel;
+    model.company_fax = data.company_fax;
+    model.sale_category = data.sale_category;
+    model.website = data.website;
+    model.contract_person = data.contract_person;
+    model.contract_phone = data.contract_phone;
+    model.contract_email = data.contract_email;
+    model.content_id = data.content_id;
+    model.create_person = data.create_person;
+    model.create_date = data.create_date;
+    model.update_person = data.update_person;
+    model.update_date = data.update_date;
     model.save(callback);
 }
