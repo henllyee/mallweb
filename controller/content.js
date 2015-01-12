3b92e7782bde6c283863440cdf6b119445df3297
henrycui <henllyee@126.com>
2015-01-12 13:56:45 +0800

    convert controller modify

--- controller/content.js
+++ controller/content.js
@@ -13,4 +13,17 @@ router.get('/convert/:tid',function(req,res,next){
     });
 });
 
+router.post('/convert',function(req,res,next){
+   var data = {
+       require_data : req.body.require_data,
+       require_content : req.body.require_content,
+       website_id : req.body.website_id,
+       content_id : req.body.content_id,
+       customer_id : req.body.customer_id,
+       create_user : req.body.create_user,
+       create_date : req.body.create_date,
+       remark : req.body.remark
+   };
+});
+
 module.exports = router;
